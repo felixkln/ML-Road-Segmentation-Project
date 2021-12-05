005888c6ebaede488b87d5a060abdbe93db6ab02
felixkln <felix.klein64@gmail.com>
2021-12-05 22:09:12 +0100

    transforms file 2

--- Felix/transforms.py
+++ Felix/transforms.py
@@ -19,12 +19,14 @@ class AddGaussianNoise(object):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
 
-transform = transforms.Compose([
+transform1 = transforms.Compose([
     transforms.ToPILImage(),
     transforms.RandomCrop((100, 100)),  # dividing images in 16 pieces
     transforms.RandomCrop((200, 200)),  # dividing images in 4 pieces
     transforms.RandomVerticalFlip(0.5),  # flipping with probability of 0.5
     transforms.RandomHorizontalFlip(0.5),
+    transforms.RandomRotation(90, expand=True),
+    transforms.RandomRotation(45, expand=True),
     transforms.ColorJitter(brightness=0.1, contrast=0.2, saturation=0, hue=0),
     transforms.ToTensor(),
     AddGaussianNoise(0.0, 0.2)  # custom class adding gaussian noise
