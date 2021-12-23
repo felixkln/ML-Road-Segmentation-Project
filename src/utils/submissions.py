75ef30f47d349c7689ecc3010374f958072648a9
galannp <82237388+galannp@users.noreply.github.com>
2021-12-23 13:12:26 +0100

    Update submissions.py

--- src/utils/submissions.py
+++ src/utils/submissions.py
@@ -23,19 +23,15 @@ def train_submissions(model, criterion, dataset_train, optimizer, num_epochs, de
     model : torch.nn.Module
       Model used for the prediction (a CNN for instance)
     criterion : torch.nn.modules.loss._Loss
-      Type of loss used as metric
+      Type of loss to optimize (CrossEntropyLoss for instance)
     dataset_train : torch.utils.data.DataLoader
       Loaded training dataset
     optimizer : torch.optim.Optimizer
-      Type of method used to minimize the losses (GD, SGD, ADAM, ISTA, FISTA, ADAGRAD, etc.)
+      Type of method used to minimize the loss (GD, SGD, ADAM, ISTA, FISTA, ADAGRAD, etc.)
     num_epochs : int
       Number of iterations over the total dataset
     device : torch.cuda
       If available, this function uses the GPU of your computer to accelerate the running time
-
-    Returns
-    -------
-    nothing
 
     """
     print("Starting training")
@@ -44,7 +40,7 @@ def train_submissions(model, criterion, dataset_train, optimizer, num_epochs, de
     loss_train = []
     for epoch in range(num_epochs):
         # Train an epoch
-        running_loss = 0.
+        running_loss = 0. # training loss accumulated for one epoch
         f1_train = []
         for batch_x, batch_y in tqdm(dataset_train):
             batch_x, batch_y = batch_x.to(device), batch_y.to(device)
@@ -54,13 +50,16 @@ def train_submissions(model, criterion, dataset_train, optimizer, num_epochs, de
             loss = criterion(prediction, batch_y)
             running_loss += loss.item()
 
+            # Backpropagation
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
+            # Add the f1-score for the current training batch
             predicted_labels = np.argmax(prediction.detach().cpu(), 1)
             f1_train.append(f1_score(batch_y.detach().cpu(), predicted_labels))
 
+        # add the averaged training loss and f1-score for this epoch
         loss_train.append(running_loss / len(dataset_train))
         f1_epoch = sum(f1_train) / len(f1_train)
 
@@ -85,8 +84,8 @@ def mask_to_submission_strings(model_lenet, image_filename, patch_size, padding,
       Size of the patches (cropped images)
     padding : int
       Shift value for the cropping operation
-    mean_img, std_img : 3-tuples
-      Mean and standard deviation of each channel
+    mean_img, std_img : nd.array
+      Mean and standard deviation for each channel
 
     Returns
     -------
@@ -94,25 +93,32 @@ def mask_to_submission_strings(model_lenet, image_filename, patch_size, padding,
     """
     model_lenet.eval()
     img_number = int(re.search(r"\d+", image_filename).group(0))
+    # Load the test image
     im = np.asarray(mpimg.imread(image_filename))
     imgwidth = im.shape[0]
     imgheight = im.shape[1]
+    # Add padding to the image for extracting a frame surrounding each patch
     im = np.lib.pad(
         im, ((padding, padding), (padding, padding), (0, 0)), 'reflect')
     patch_size = 16
+    # Cropping the test satellite image
     for j in range(padding, imgwidth + padding, patch_size):
         for i in range(padding, imgheight + padding, patch_size):
             patch = im[i - padding:i + patch_size + padding,
                        j - padding:j + patch_size + padding, :]
+            # Normalizing each patch
             patch = T.Compose(
                 [T.ToTensor(), T.Normalize(mean_img, std_img)])(patch)
+            # Compute the prediction for this patch
             predict = model_lenet(patch[np.newaxis, :])
             label = np.argmax(predict.detach().numpy(), 1)[0]
+            # Add the submission line associated to the prediction for this patch
             yield("{:03d}_{}_{},{}".format(img_number, j - padding, i - padding, label))
 
 
 def masks_to_submission(model_lenet, submission_filename, image_filenames, patch_size, padding, mean_img, std_img):
-    """Converts images into a submission file
+    """Converts test images into a submission file for AICrowd website
+    Generates the submission file 'submission.csv' containing all the predictions corresponding to each patch of each test image
 
     Parameters
     ----------
@@ -126,15 +132,13 @@ def masks_to_submission(model_lenet, submission_filename, image_filenames, patch
       Size of the patches (cropped images)
     padding : int
       Shift value for the cropping operation
-    mean_img, std_img : 3-tuples
+    mean_img, std_img : nd.array
       Mean and standard deviation of each channel
 
-    Returns
-    -------
-    a .csv file with the predictions corresponding to each patch of each image
     """
     with open(submission_filename, 'w') as f:
         f.write('id,prediction\n')
         for fn in tqdm(image_filenames):
+            # Generate the prediction lies for one satellite image
             f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(
                 model_lenet.cpu(), fn, patch_size, padding, mean_img, std_img))
